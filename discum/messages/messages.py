efaf443479e5e73e05138dadf34c140e0d4a6650
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2020-08-08 13:45:50 -0400

    added a function to search/get all messages
    def getMessages,
    feel the need to scrape all the messages ever sent on a server?, use this function

--- discum/messages/messages.py
+++ discum/messages/messages.py
@@ -2,18 +2,119 @@ import requests
 import json
 from ..fileparse.fileparse import Fileparse
 from ..Logger import *
-from urllib.parse import urlparse
+from urllib.parse import urlparse,quote_plus
 from requests_toolbelt import MultipartEncoder
 import random,string
+import math
 import os
+import time
 
 class Messages(object):
 	def __init__(self, discord, s): #s is the requests session object
 		self.discord = discord
 		self.s = s
 
-	#get messages
-	def getMessage(self,channelID,num): # num <= 100
+	#search messages (can load all messages or search thru them), talk about mining some data lol
+	def getMessages(self,guildID,channelID,userID,mentionsUserID,has,beforeDate,afterDate,textSearch):
+		url = self.discord+"guilds/"+guildID+"/messages/search?"
+		queryparams = ""
+		if not all(v is None for v in [channelID,userID,mentionsUserID,has,beforeDate,afterDate,textSearch]):
+			if channelID != None and isinstance(channelID,list):
+				for item in channelID:
+					if isinstance(item,int):
+						queryparams += "channel_id="+str(item)+"&"
+					elif isinstance(item,str) and len(item)>0:
+						queryparams += "channel_id="+item+"&"
+			if userID != None and isinstance(userID,list):
+				for item in userID:
+					if isinstance(item,int):
+						queryparams += "author_id="+str(item)+"&"
+					elif isinstance(item,str) and len(item)>0:
+						queryparams += "author_id="+item+"&"
+			if mentionsUserID != None and isinstance(mentionsUserID,list):
+				for item in mentionsUserID:
+					if isinstance(item,int):
+						queryparams += "mentions="+str(item)+"&"
+					elif isinstance(item,str) and len(item)>0:
+						queryparams += "mentions="+item+"&"
+			if has != None and isinstance(has,list):
+				for item in has:
+					if isinstance(item,str) and len(item)>0:
+						queryparams += "has="+item+"&"
+			if beforeDate != None and isinstance(beforeDate,int):
+				queryparams += "min_id="+str(beforeDate)+"&"
+			if afterDate != None and isinstance(afterDate,int):
+				queryparams += "max_id="+str(afterDate)+"&"
+			if textSearch != None and isinstance(textSearch,str): #textSearch can be len 0....ugh
+				queryparams += "content="+quote_plus(textSearch)
+			url += queryparams
+			Logger.LogMessage('Get -> {}'.format(url))
+			response = self.s.get(url) #but wait, discord's gonna be an asshole and only give us the first 25 results, rip heres a loop
+			displayedresponse = response.json() #after this...
+			if 'messages' in displayedresponse: #if request response not 400
+				displayedresponse['messages'] = '...' #cause console space
+			if 'total_results' in response.json(): #now onto getting all the messages...note for old/active servers this can be a pain in the ass
+				actualresponse = response.json() #youve got to be shitting me
+				actualresponse['messages'] = [] #just removing all the shit
+				total_pages = math.ceil(response.json()['total_results']/25) #this changes...............
+				n=0 #page num
+				while n<total_pages:
+					nextpageurl = url + "&offset="+str(n*25)
+					Logger.LogMessage('Get -> {}'.format(nextpageurl))
+					nextpageresponse = self.s.get(nextpageurl)
+					if 'retry_after' in nextpageresponse.json():
+						print('rate limited: waiting for '+str(nextpageresponse.json()['retry_after']/1000)+' seconds')
+						time.sleep(nextpageresponse.json()['retry_after']/1000 + 1) #add an extra second just in case
+						nextpageresponse = self.s.get(nextpageurl) #run that shit again
+					total_pages = math.ceil(nextpageresponse.json()['total_results']/25) #update total_pages in case it changed
+					Logger.LogMessage('Response <- {}'.format("next 25 messages loaded"), log_level=LogLevel.OK)
+					hitresponses = [] # now...discord's shit never ends cause nextpageresponse.json()['messages'] is a list of length something and includes some junk....so we have to get rid of that junk
+					for k in range(len(nextpageresponse.json()['messages'])): #for every group
+						for j in range(len(nextpageresponse.json()['messages'][k])): #for every item in every group
+							if 'hit' in nextpageresponse.json()['messages'][k][j]: #fml
+								hitresponses.append(nextpageresponse.json()['messages'][k][j])
+								print('1')
+								print(nextpageresponse.json()['messages'][k][j]) ####debug line, remove if u want*****
+					actualresponse['messages'].extend(hitresponses)
+					n+=1 #onto the next page
+				actualresponse['messages'] = list({v['id']:v for v in actualresponse['messages']}.values()) #in case duplicates formed, thank u stackexchange
+				actualresponse['total_results'] = len(actualresponse['messages']) #update total results num
+				return actualresponse
+		Logger.LogMessage('Get -> {}'.format(url))
+		response = self.s.get(url)
+		displayedresponse = response.json()
+		if 'messages' in displayedresponse: #if request response not 400
+			displayedresponse['messages'] = '...' #cause total results can go well over 10k
+		if 'total_results' in response.json(): #now onto getting all the messages...note for old/active servers this can be a pain in the ass
+			actualresponse = response.json() #youve got to be shitting me
+			actualresponse['messages'] = [] #just removing all the shit
+			total_pages = math.ceil(response.json()['total_results']/25) #this changes
+			n=0 #page num
+			while n<total_pages:
+				nextpageurl = url + "&offset="+str(n*25)
+				Logger.LogMessage('Get -> {}'.format(nextpageurl))
+				nextpageresponse = self.s.get(nextpageurl)
+				if 'retry_after' in nextpageresponse.json():
+					print('rate limited: waiting for '+str(nextpageresponse.json()['retry_after']/1000)+' seconds')
+					time.sleep(nextpageresponse.json()['retry_after']/1000 + 1) #add an extra second just in case
+					nextpageresponse = self.s.get(nextpageurl) #run that shit again
+				total_pages = math.ceil(nextpageresponse.json()['total_results']/25) #update total_pages in case it changed
+				Logger.LogMessage('Response <- {}'.format("next 25 messages loaded"), log_level=LogLevel.OK)
+				hitresponses = [] # now...discord's shit never ends cause nextpageresponse.json()['messages'] is a list of length something and includes some junk....so we have to get rid of that junk
+				for k in range(len(nextpageresponse.json()['messages'])): #for every group
+					for j in range(len(nextpageresponse.json()['messages'][k])): #for every item in every group
+						if 'hit' in nextpageresponse.json()['messages'][k][j]:
+							hitresponses.append(nextpageresponse.json()['messages'][k][j])
+							print('2')
+							print(nextpageresponse.json()['messages'][k][j]) ####debug line, remove if u want*****
+				actualresponse['messages'].extend(hitresponses)
+				n+=1 #onto the next page
+			actualresponse['messages'] = list({v['id']:v for v in actualresponse['messages']}.values()) #in case duplicates formed, thank u stackexchange
+			actualresponse['total_results'] = len(actualresponse['messages']) #update total results num
+			return actualresponse
+
+	#get recent messages (up to 100)
+	def getRecentMessage(self,channelID,num): # num <= 100
 		url = self.discord+"channels/"+channelID+"/messages?limit="+str(num)
 		Logger.LogMessage('Get -> {}'.format(url))
 		response = self.s.get(url)

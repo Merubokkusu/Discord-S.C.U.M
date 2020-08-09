009854a3adbcdba93ee1f259a06bb02f51a7d307
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2020-08-08 22:43:52 -0400

    added basic message management
    check wiki

--- discum/messages/messages.py
+++ discum/messages/messages.py
@@ -165,3 +165,91 @@ class Messages(object):
 		response = self.s.post(url, data=m)
 		Logger.LogMessage('Response <- {}'.format(response.text), log_level=LogLevel.OK)
 		return response
+
+	def searchMessages(self,guildID,channelID,userID,mentionsUserID,has,beforeDate,afterDate,textSearch,afterNumResults): #same as the search feature on servers, afterNumResults is multiples of 25 and indicates after which messages (type int)
+		url = self.discord+"guilds/"+guildID+"/messages/search?"
+		queryparams = ""
+		if not all(v is None for v in [channelID,userID,mentionsUserID,has,beforeDate,afterDate,textSearch,afterNumResults]):
+			if channelID != None and isinstance(channelID,list):
+				for item in channelID:
+					if isinstance(item,int):
+						queryparams += "channel_id="+str(item)+"&"
+					elif isinstance(item,str) and len(item)>0:
+						queryparams += "channel_id="+item+"&"
+			if userID != None and isinstance(userID,list):
+				for item in userID:
+					if isinstance(item,int):
+						queryparams += "author_id="+str(item)+"&"
+					elif isinstance(item,str) and len(item)>0:
+						queryparams += "author_id="+item+"&"
+			if mentionsUserID != None and isinstance(mentionsUserID,list):
+				for item in mentionsUserID:
+					if isinstance(item,int):
+						queryparams += "mentions="+str(item)+"&"
+					elif isinstance(item,str) and len(item)>0:
+						queryparams += "mentions="+item+"&"
+			if has != None and isinstance(has,list):
+				for item in has:
+					if isinstance(item,str) and len(item)>0:
+						queryparams += "has="+item+"&"
+			if beforeDate != None and isinstance(beforeDate,int):
+				queryparams += "min_id="+str(beforeDate)+"&"
+			if afterDate != None and isinstance(afterDate,int):
+				queryparams += "max_id="+str(afterDate)+"&"
+			if textSearch != None and isinstance(textSearch,str): #textSearch can be len 0....ugh
+				queryparams += "content="+quote_plus(textSearch)+"&"
+			if afterNumResults != None and isinstance(afterNumResults,int):
+				queryparams += "offset="+str(afterNumResults)
+			url += queryparams
+			Logger.LogMessage('Get -> {}'.format(url))
+			response = self.s.get(url)
+			Logger.LogMessage('Response <- {}'.format(response.text), log_level=LogLevel.OK)
+			return response
+		Logger.LogMessage('Get -> {}'.format(url))
+		response = self.s.get(url)
+		Logger.LogMessage('Response <- {}'.format(response.text), log_level=LogLevel.OK)
+		return response
+
+	def typingAction(self,channelID): #sends the typing action for 10 seconds (or until you change the page)
+		url = self.discord+"channels/"+channelID+"/typing"
+		Logger.LogMessage('Post -> {}'.format(url))
+		response = self.s.post(url)
+		Logger.LogMessage('Response <- {}'.format(response.text), log_level=LogLevel.OK)
+		return response
+
+	def editMessage(self,channelID,messageID,newMessage):
+		url = self.discord+"channels/"+channelID+"/messages/"+messageID
+		body = {"content": newMessage}
+		Logger.LogMessage('Patch -> {}'.format(url))
+		Logger.LogMessage('{}'.format(str(body)))
+		response = self.s.patch(url, data=json.dumps(body))
+		Logger.LogMessage('Response <- {}'.format(response.text), log_level=LogLevel.OK)
+		return response
+
+	def deleteMessage(self,channelID,messageID):
+		url = self.discord+"channels/"+channelID+"/messages/"+messageID
+		Logger.LogMessage('Delete -> {}'.format(url))
+		response = self.s.delete(url)
+		Logger.LogMessage('Response <- {}'.format(response.text), log_level=LogLevel.OK)
+		return response
+
+	def pinMessage(self,channelID,messageID):
+		url = self.discord+"channels/"+channelID+"/pins/"+messageID
+		Logger.LogMessage('Put -> {}'.format(url))
+		response = self.s.put(url)
+		Logger.LogMessage('Response <- {}'.format(response.text), log_level=LogLevel.OK)
+		return response
+
+	def unPinMessage(self,channelID,messageID):
+		url = self.discord+"channels/"+channelID+"/pins/"+messageID
+		Logger.LogMessage('Delete -> {}'.format(url))
+		response = self.s.delete(url)
+		Logger.LogMessage('Response <- {}'.format(response.text), log_level=LogLevel.OK)
+		return response
+
+	def getPins(self,channelID): #get pinned messages
+		url = self.discord+"channels/"+channelID+"/pins"
+		Logger.LogMessage('Get -> {}'.format(url))
+		response = self.s.get(url)
+		Logger.LogMessage('Response <- {}'.format(response.text), log_level=LogLevel.OK)
+		return response

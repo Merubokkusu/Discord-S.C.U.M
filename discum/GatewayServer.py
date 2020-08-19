ea89dd2c0c2f2fb52289fcfa948a0e2f7c6ce2cc
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2020-08-19 00:26:58 -0400

    see description, made a lot of changes
    greatly simplified gatewayserver, used asyncio, added customizable websocket tasks (for example, after initializing gatewayserver, you can now do:
    gateway.runIt({
      1: {
        "send": [{
          "op": 4,
          "d": {
            "guild_id": None,
            "channel_id": "745427469607108671",
            "self_mute": False,
            "self_deaf": False,
            "self_video": False
          }
        }],
        "receive": [{
          "t": "VOICE_SERVER_UPDATE"
        }]
      }
    })
    
    so what does this do? 
    After sending the auth data, receiving the session data, and taking care of the first heartbeat, some data will be sent to the server (indicated by the "send" key above) and until a message with "t" = "VOICE_SERVER_UPDATE" comes back, the program will keep on running. Also, notice the 1 key. This is because you can have multiple tasks. For this example (sending a call request), only 1 task is needed, but in case you need to do multiple tasks this might be helpful for you.

--- discum/GatewayServer.py
+++ discum/GatewayServer.py
@@ -1,25 +1,19 @@
-import websocket
-import ssl
-from .erlang import *
-from .erlang import etfDictToJson,getName #just in case
-from .Logger import *
+import asyncio
+import websockets
+import json
+
+class TaskCompleted(Exception):
+    pass
 
 class GatewayServer():
-    '''
-    Manages authentication and data transfer to/from http://gateway.discord.gg
-    '''
-    URL = "wss://gateway.discord.gg/?encoding=etf&v=6"
 
-    class OPCODE:
-        '''
-        Taken from https://discordapp.com/developers/docs/topics/opcodes-and-status-codes#voice-opcodes
-        '''
+    class OPCODE: #https://discordapp.com/developers/docs/topics/opcodes-and-status-codes
         # Name                  Code    Client Action   Description
         DISPATCH =              0  #    Receive         dispatches an event
         HEARTBEAT =             1  #    Send/Receive    used for ping checking
         IDENTIFY =              2  #    Send            used for client handshake
-        STATUS =                3  #    Send            used to update the client status
-        VOICE_STATE =           4  #    Send            used to join/move/leave voice channels
+        STATUS_UPDATE =         3  #    Send            used to update the client status
+        VOICE_UPDATE =          4  #    Send            used to join/move/leave voice channels
         #                       5  #    ???             ???
         RESUME =                6  #    Send            used to resume a closed connection
         RECONNECT =             7  #    Receive         used to tell clients to reconnect to the gateway
@@ -29,62 +23,224 @@ class GatewayServer():
         HEARTBEAT_ACK =         11 #    Sent immediately following a client heartbeat that was received
         GUILD_SYNC =            12 #    ???             ???
 
-    def __init__(self, proxy_host, proxy_port):
-        self.__proxy_host = proxy_host
-        self.__proxy_port = proxy_port
-        self.__ws = websocket.WebSocket(sslopt={"check_hostname": False, "cert_reqs": ssl.CERT_NONE})
-        self.__session_settings = {}
-        self.__ws.settimeout(5)
-        
-    def Connect(self, token):
-        gateway_auth_data = {OtpErlangBinary(str.encode('op'),bits=8): 2,
-                OtpErlangBinary(str.encode('d'),bits=8):
-                    {OtpErlangBinary(str.encode('properties'),bits=8):
-                        {OtpErlangBinary(str.encode('client_version'),bits=8): OtpErlangBinary(str.encode('0.0.305'),bits=8),
-                        OtpErlangBinary(str.encode('os'),bits=8): OtpErlangBinary(str.encode('Windows'),bits=8),
-                        OtpErlangBinary(str.encode('os_version'),bits=8): OtpErlangBinary(str.encode('6.1.7601'),bits=8),
-                        OtpErlangBinary(str.encode('os_arch'),bits=8): OtpErlangBinary(str.encode('x64'),bits=8),
-                        OtpErlangBinary(str.encode('release_channel'),bits=8): OtpErlangBinary(str.encode('stable'),bits=8),
-                        OtpErlangBinary(str.encode('client_build_number'),bits=8): 44004,
-                        OtpErlangBinary(str.encode('browser'),bits=8): OtpErlangBinary(str.encode('Discord Client'),bits=8),
-                        OtpErlangBinary(str.encode('client_event_source'),bits=8): OtpErlangAtom('nil')
-                        },
-                    OtpErlangBinary(str.encode('compress'),bits=8): False,
-                    OtpErlangBinary(str.encode('token'),bits=8): OtpErlangBinary(str.encode("{}".format(token)), bits=8),
-                    OtpErlangBinary(str.encode('presence'),bits=8):
-                        {OtpErlangBinary(str.encode('status'),bits=8): OtpErlangBinary(str.encode('online'),bits=8),
-                        OtpErlangBinary(str.encode('since'),bits=8): 0,
-                        OtpErlangBinary(str.encode('afk'),bits=8): False,
-                        OtpErlangBinary(str.encode('activities'),bits=8): []
-                        }
-                    }
-                }
-        # Connect to gateway
-        gateway_auth_data = term_to_binary(gateway_auth_data)
-        if self.__proxy_host != False and self.__proxy_port != False:
-            self.__ws.connect(self.URL, origin="https://discordapp.com", http_proxy_host=self.__proxy_host, http_proxy_port=self.__proxy_port, header={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36"})
+    def __init__(self, token, proxy_host, proxy_port):
+        self.proxy_host = proxy_host
+        self.proxy_port = proxy_port
+        self.interval = None
+        self.sequence = None
+        self.session_id = None
+        self.session_data = None
+        self.checkit = {} #see receive function
+        self.task_data = []
+        self.taskCompleted = False
+        self.mainTaskCompleted = False
+        self.taskCompleteConstant = {}
+        self.stopIt = False
+        self.taskComplete = {}
+        self.mainTaskComplete = {}
+        self.all_tasks = {}
+
+        self.auth = {
+                "token": token,
+                "properties": {
+                    "os": "Windows",
+                    "browser": "Chrome",
+                    "device": "",
+                },
+                "large_threshold": 150,
+                "synced_guilds": [],
+                "presence": {
+                    "status": "online",
+                    "since": 0,
+                    "afk": False,
+                    "game": None
+                },
+                "compress": False
+            }
+
+        self.loop = asyncio.get_event_loop()
+
+        #variables for calling
+        self.channelID = None
+        self.userID = None
+        self.media_token = None
+        self.call_endpoint = None
+        self.call_session_id = None
+
+
+    def NestedDictValues(self,d):
+        for v in d.values():
+            if isinstance(v, dict):
+                yield from self.NestedDictValues(v)
+            else:
+                yield v
+
+    def runIt(self,tasks):
+        self.all_tasks = tasks
+        if self.all_tasks != 'get session data':
+            self.mainTaskComplete = {key: None for key in self.all_tasks.keys()}
         else:
-            self.__ws.connect(self.URL, origin="https://discordapp.com", header={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.305 Chrome/69.0.3497.128 Electron/4.0.8 Safari/537.36"})
-        Logger.LogMessage('Gateway_wss connecting -> {}'.format(self.URL), log_level=LogLevel.OK)
+        	self.mainTaskComplete = {1:'get session data'}
 
-        # Receive response
-        response = str(self.__ws.recv())
-        Logger.LogMessage('Gateway_wss received <- {} bytes'.format(len(response)))
-        Logger.LogMessage('Gateway_wss data received \r\n', to_file=False, to_console=False, hex_data=response)
+        try:
+            asyncio.run(self.main())
+        except TaskCompleted:
+            self.loop.stop()
 
-        # Send auth message
-        Logger.LogMessage('Gateway_wss sending -> {} bytes'.format(len(gateway_auth_data)))
-        self.__ws.send_binary(gateway_auth_data)
-        Logger.LogMessage('Gateway_wss data sent -> \r\n', to_file=False, to_console=False, hex_data=gateway_auth_data)
+    async def taskManager(self):
+        if self.all_tasks != 'get session data':
+            for index in self.all_tasks:
+                print('task num: '+str(index))
+                await self.addTask(self.all_tasks[index])
+                while self.mainTaskCompleted == False:
+                    await asyncio.sleep(1)
+                self.mainTaskComplete[index] = "complete"
+        else:
+            while self.session_data is None:
+                await asyncio.sleep(1)
+            self.mainTaskComplete[1] = 'complete'
 
-        # Receive session settings
-        response = self.__ws.recv()
-        etf_data = binary_to_term(response)
-        Logger.LogMessage('Gateway_wss received <- {} bytes'.format(len(response)))
-        Logger.LogMessage('Gateway_wss data received <- \r\n', to_file=False, to_console=False, hex_data=response)
-        etfDictToJson(self.__session_settings, etf_data[OtpErlangAtom(str.encode('d'))]) #self.__session_settings has a lot of important data, not to mention free server.members yay
-        Logger.LogMessage('Authenticated over gateway as {}'.format(self.__session_settings[str.encode('user')][str.encode('id')]), log_level=LogLevel.OK)
-        Logger.LogMessage('Session id: {}'.format(self.__session_settings[str.encode('session_id')].decode("utf-8")))
-        
-    def return_session_settings():
-        return self.__session_settings
+    async def main(self): # http_proxy_host=self.proxy_host, http_proxy_port=self.proxy_port
+        if self.proxy_host == None:
+            async with websockets.connect(
+                    'wss://gateway.discord.gg/?v=6&encoding=json', origin="https://discordapp.com") \
+                    as self.websocket:
+                await self.hello()
+                if self.interval is None:
+                    print("Hello failed, exiting")
+                    return
+                await asyncio.gather(self.heartbeat(), self.receive(),self.stopLoop(),self.taskManager())
+        else:
+            async with websockets.connect(
+                    'wss://gateway.discord.gg/?v=6&encoding=json', origin="https://discordapp.com", host=self.proxy_host, port=self.proxy_port) \
+                    as self.websocket:
+                await self.hello()
+                if self.interval is None:
+                    print("Hello failed, exiting")
+                    return
+                await asyncio.gather(self.heartbeat(), self.receive(),self.stopLoop(),self.taskManager())
+
+    async def receive(self):
+        print("Entering receive")
+        async for message in self.websocket:
+            self.taskComplete = self.taskCompleteConstant
+            print("<", message)
+            #print("Recieved message from server.")
+            data = json.loads(message)
+            if data["op"] == self.OPCODE.DISPATCH:
+                self.sequence = int(data["s"])
+                event_type = data["t"]
+                if event_type == "READY":
+                    self.session_id = data["d"]["session_id"]
+                    self.session_data = data
+                if event_type == "VOICE_SERVER_UPDATE":
+                    if "token" in data["d"]:
+                        self.media_token = data["d"]["token"]
+                    if "endpoint" in data["d"]:
+                        endpointdata = data["d"]["endpoint"].split(':')[:][0]
+                        self.call_endpoint = "wss://{}/?v=5".format(endpointdata)
+                if event_type == "VOICE_STATE_UPDATE":
+                    if "user_id" in data["d"]:
+                        self.userID = data["d"]["user_id"]
+                    if "session_id" in data["d"]:
+                        self.call_session_id = data["d"]["session_id"]
+            if "op" in self.taskComplete and self.task["op"] == data["op"]:
+                self.taskComplete["op"] = "complete"
+            if "d" in self.taskComplete:
+                if "key" in self.taskComplete["d"] and self.task["d"]["key"] in data["d"]:
+                    self.taskComplete["d"]["key"] = "complete"
+                if "value" in self.taskComplete["d"] and self.task["d"]["value"] in data["d"].values():
+                    self.taskComplete["d"]["value"] = "complete"
+                if "text" in self.taskComplete["d"] and self.task["d"]["text"] in str(data["d"]):
+                    self.taskComplete["d"]["text"] = "complete"
+            if "s" in self.taskComplete and self.task["s"] == data["s"]:
+                self.taskComplete["s"] = "complete"
+            if "t" in self.taskComplete and self.task["t"] == data["t"]:
+                self.taskComplete["t"] = "complete"
+            if "count" in self.taskComplete and "complete" in list(self.taskComplete.values()):
+                self.current_count+=1
+                if self.current_count == self.task["count"]:
+                    self.taskComplete["count"] = "complete"
+            if all(value == "complete" for value in list(self.NestedDictValues(self.taskComplete))):
+                self.current_count = 0 #reset count
+                self.task_data.append(data) #this is only accurate for checking len(self.task_data). the data itself can be inaccurate if there are multiple simultaneous responses
+                print(self.task_data)
+                self.taskCompleted = True 
+                if len(self.task_data) == len(self.checkit):
+                    self.mainTaskCompleted = True
+
+    async def send(self, opcode, payload):
+        data = self.opcode(opcode, payload)
+        print(">", data)
+        await self.websocket.send(data)
+
+    async def heartbeat(self):
+        print("Entering heartbeat")
+        while self.interval is not None:
+            print("Sending a heartbeat")
+            await self.send(self.OPCODE.HEARTBEAT, self.sequence)
+            await asyncio.sleep(self.interval)
+
+    async def hello(self):
+        await self.send(self.OPCODE.IDENTIFY, self.auth)
+        print(f"hello > auth")
+
+        ret = await self.websocket.recv()
+        print(f"hello < {ret}")
+
+        data = json.loads(ret)
+        opcode = data["op"]
+        if opcode != 10:
+            print("Unexpected reply")
+            print(ret)
+            return
+        self.interval = (data["d"]["heartbeat_interval"] - 2000) / 1000
+        print("interval:", self.interval)
+
+    def opcode(self, opcode: int, payload) -> str:
+        data = {
+            "op": opcode,
+            "d": payload
+        }
+        return json.dumps(data)
+
+    async def addTask(self,data):
+        while self.taskCompleted == False:
+            await asyncio.sleep(1)
+        self.taskCompleted = False #reset
+        sendData = data["send"]
+        for mail in sendData: #send data if theres data to send
+            #if you want to make changes to mail make that here
+            await self.send(mail["op"],mail["d"])
+        receiveData = data["receive"]
+        self.checkit = receiveData
+        self.task_data = [] #clear it
+        for search in receiveData: #receive data is essentially a search function (look at async def receive)
+            self.taskCompleted = False
+            #task processing
+            self.task = search
+            self.keylist = self.task.keys()
+            self.taskComplete = {}
+            self.current_count = 0
+            if "op" in self.task:
+                self.taskComplete["op"] = None
+            if "d" in self.task:
+                self.taskComplete["d"] = {}
+                if "key" in self.task["d"]:
+                    self.taskComplete["d"]["key"] = None
+                if "value" in self.task["d"]:
+                    self.taskComplete["d"]["value"] = None
+                if "text" in self.task["d"]:
+                    self.taskComplete["d"]["text"] = None
+            if "s" in self.task:
+                self.taskComplete["s"] = None
+            if "t" in self.task:
+                self.taskComplete["t"] = None
+            if "count" in self.task:
+                self.taskComplete["count"] = None
+            self.taskCompleteConstant = self.taskComplete
+
+    async def stopLoop(self):
+        while not all(value == "complete" for value in self.mainTaskComplete.values()):
+            await asyncio.sleep(1)
+        raise TaskCompleted(self.all_tasks)

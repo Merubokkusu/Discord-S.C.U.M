925a89e24c673030c38642dede690579d0c0ace6
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2020-11-18 00:37:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ setup(
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',

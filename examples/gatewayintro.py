a49b902ec41d9891183ce218881f64240781e007
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-01-04 15:17:37 -0500

    Create gatewayintro.py

---
+++ examples/gatewayintro.py
@@ -0,0 +1,18 @@
+import discum
+bot = discum.Client(token='ur token')
+
+@bot.gateway.command
+def helloworld(resp):
+    if resp.event.ready_supplemental: #ready_supplemental is sent after ready
+        user = bot.gateway.session.user
+        print("Logged in as {}#{}".format(user['username'], user['discriminator']))
+    if resp.event.message:
+        m = resp.parsed.auto()
+        guildID = m['guild_id'] if 'guild_id' in m else None #because DMs are technically channels too
+        channelID = m['channel_id']
+        username = m['author']['username']
+        discriminator = m['author']['discriminator']
+        content = m['content']
+        print("> guild {} channel {} | {}#{}: {}".format(guildID, channelID, username, discriminator, content))
+
+bot.gateway.run(auto_reconnect=True)

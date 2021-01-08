af1c05ec2dc744e5d9457741a19f0543d5f28615
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-01-07 21:28:54 -0500

    Update parse.py
    https://discord.com/developers/docs/resources/channel#message-object-message-types

--- discum/gateway/messages/parse.py
+++ discum/gateway/messages/parse.py
@@ -2,24 +2,24 @@ class MessageParse(object):
 	@staticmethod
 	def message_create(response):
 		message = dict(response["d"])
-		types = [
-		    "default",
-		    "recipient_added",
-		    "recipient_removed",
-		    "call",
-		    "channel_name_changed",
-		    "channel_icon_changed",
-		    "channel_message_pinned",
-		    "guild_member_joined",
-		    "user_premium_guild_subscription",
-		    "user_premium_guild_subscription_tier_1",
-		    "user_premium_guild_subscription_tier_2",
-		    "user_premium_guild_subscription_tier_3",
-		    "channel_follow_added",
-		    "guild_discovery_disqualified",
-		    "guild_discovery_requalified",
-		    "reply",
-		    "application_command"
-		]
+		types = {
+		    0: "default",
+		    1: "recipient_added",
+		    2: "recipient_removed",
+		    3: "call",
+		    4: "channel_name_changed",
+		    5: "channel_icon_changed",
+		    6: "channel_message_pinned",
+		    7: "guild_member_joined",
+		    8: "user_premium_guild_subscription",
+		    9: "user_premium_guild_subscription_tier_1",
+		    10: "user_premium_guild_subscription_tier_2",
+		    11: "user_premium_guild_subscription_tier_3",
+		    12: "channel_follow_added",
+		    14: "guild_discovery_disqualified",
+		    15: "guild_discovery_requalified",
+		    19: "reply",
+		    20: "application_command"
+		}
 		message["type"] = types[response["d"]["type"]] #number to str
 		return message

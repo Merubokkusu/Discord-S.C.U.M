e58f612e59286003a078ba1f590823140164327e
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-01-11 10:16:42 -0500

    Update login.py

--- discum/start/login.py
+++ discum/start/login.py
@@ -4,13 +4,10 @@ class Login:
     '''
     Manages HTTP authentication
     '''
-    def __init__(self, s, discordurl, user_email, user_password, log):
+    def __init__(self, s, discordurl, log):
         self.s = s
         self.discord = discordurl
-        self.__user_email = user_email
-        self.__user_password = user_password
         self.log = log
-        self.__token = None
 
     def GetXFingerprint(self):
         url = self.discord + "experiments"
@@ -18,15 +15,18 @@ class Login:
         xfingerprint = json.loads(reqxfinger.content)['fingerprint']
         return xfingerprint
 
-    def Connect(self):
+    def GetToken(self, email, password, undelete=False, captcha=None, source=None, gift_code_sku_id=None):
         url = self.discord + "auth/login"
         self.xfingerprint = self.GetXFingerprint()
         self.s.headers.update({"X-Fingerprint": self.xfingerprint})
-        body = {"email": self.__user_email, "password": self.__user_password, "undelete": False, "captcha_key": None, "login_source": None, "gift_code_sku_id": None}
+        body = {
+            "email": email,
+            "password": password,
+            "undelete": undelete,
+            "captcha_key": captcha,
+            "login_source": source,
+            "gift_code_sku_id": gift_code_sku_id,
+        }
         response = Wrapper.sendRequest(self.s, 'post', url, body, log=self.log)
         self.__token = json.loads(response.content)['token']
-
-    def GetToken(self):
-        if self.__token is None:
-            self.Connect()
         return self.__token, self.xfingerprint

9a24b3bf8681071e1e746540df49de92cc4ac683
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-02-07 23:01:09 -0500

    Update gettingGuildMembers.py

--- examples/gettingGuildMembers.py
+++ examples/gettingGuildMembers.py
@@ -17,7 +17,7 @@ def close_after_fetching(resp, guild_id):
 		bot.gateway.close()
 
 def get_members(guild_id, channel_id):
-	bot.gateway.fetchMembers(guild_id, channel_id)
+	bot.gateway.fetchMembers(guild_id, channel_id, keep="all", wait=1) #get all user attributes, wait 1 second between requests
 	bot.gateway.command({'function': close_after_fetching, 'params': {'guild_id': guild_id}})
 	bot.gateway.run()
 	return bot.gateway.session.guild(guild_id).members

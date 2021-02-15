fdc5c8ad702bae09cea09de73241a006f3b8a9ce
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-02-15 11:17:16 -0500

    Update gettingGuildMembers.py

--- examples/gettingGuildMembers.py
+++ examples/gettingGuildMembers.py
@@ -20,6 +20,7 @@ def get_members(guild_id, channel_id):
 	bot.gateway.fetchMembers(guild_id, channel_id, keep="all", wait=1) #get all user attributes, wait 1 second between requests
 	bot.gateway.command({'function': close_after_fetching, 'params': {'guild_id': guild_id}})
 	bot.gateway.run()
+	bot.gateway.resetSession() #saves 10 seconds when gateway is run again
 	return bot.gateway.session.guild(guild_id).members
 
 members = get_members('322850917248663552', '754536220826009670') #yes, the channel_id input is required

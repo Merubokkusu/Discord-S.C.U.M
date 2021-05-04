b56f18ae8e3c923fd2b0826b06b9fdce9b48ec6e
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-05-04 17:44:41 -0400

    Update setup.py
    1.2.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/Merubokkusu/Discord-S.C.U.M'
 EMAIL = 'loser@merubokkusu.com'
 AUTHOR = 'Merubokkusu'
 REQUIRES_PYTHON = '>=2.7.0'
-VERSION = '1.2.0'
+VERSION = '1.2.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [

fd68d0144f12e0fcf5adc19162f78c83a13408ed
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-05-07 21:06:24 -0400

    Update logger.py
    log now accepts boolean values as well as dictionary values

--- discum/logger.py
+++ discum/logger.py
@@ -7,6 +7,8 @@ class LogLevel:
 class Logger:
 	@staticmethod
 	def log(text, color=None, log={"console":True, "file":False}):
+		if isinstance(log, bool):
+			log = {"console":log, "file":False}
 		if log["console"]:
 			if color:
 				string = color + text + '\033[m'

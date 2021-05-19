dd8b77a2117f22e7cd26f09116726b70510ab640
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-05-18 21:24:27 -0400

    Update gettingGuildMembers.py
    updated comment

--- examples/gettingGuildMembers.py
+++ examples/gettingGuildMembers.py
@@ -4,7 +4,7 @@
 # Note, you'll need to connect to the gateway to get the member list.
 # An example usage is below. The Guild and Channel ids used are from the fortnite server (652k members, around 150k of those are actually fetchable).
 # The number of fetchable members changes from time to time.
-# https://github.com/Merubokkusu/Discord-S.C.U.M/blob/master/docs/using.md#fetch-guild-members
+# https://github.com/Merubokkusu/Discord-S.C.U.M/blob/master/docs/using/Gateway_Actions.md#gatewayfetchmembers
 
 import discum
 bot = discum.Client(token='your token')

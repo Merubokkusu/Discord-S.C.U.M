9dfd27233e39d380a7c098837c6ca6614ab7b016
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-05-28 22:13:15 -0400

    added an opcode 8 brute forcer to examples

--- examples/searchGuildMembers.py
+++ examples/searchGuildMembers.py
@@ -79,7 +79,7 @@ def finishedGuildSearch(guildIDs, query="", saveAsQueryOverride=None, userIDs=No
 ######################################################################
 #how to run:
 
-#query member search in guild(s)
+#EXAMPLE 1: query member search in guild(s)
 @bot.gateway.command
 def test(resp):
 	if resp.event.ready_supplemental:
@@ -90,10 +90,9 @@ def test(resp):
 bot.gateway.run()
 
 print(bot.gateway.guildMemberSearches)
-
 bot.gateway.clearCommands()
 
-#search for userID(s) in guild(s)
+#EXAMPLE 2: search for userID(s) in guild(s)
 @bot.gateway.command
 def test(resp):
 	if resp.event.ready_supplemental:
@@ -104,5 +103,79 @@ def test(resp):
 bot.gateway.run()
 
 print(bot.gateway.guildMemberSearches)
-
 bot.gateway.clearCommands()
+
+#EXAMPLE 3: opcode 8 brute forcer
+#not entirely random. Optimized quite a bit.
+
+import time
+
+allchars = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
+bot.qList = ["!"] #query list
+bot.gateway.guildMemberSearches = {}
+bot.gateway.resetMembersOnSessionReconnect = False #member list brute forcing can take a while
+
+def calculateOption(guildID, action): #action == 'append' or 'replace'
+	if action == 'append':
+		lastUserIDs = bot.gateway.guildMemberSearches[guildID]["queries"][''.join(bot.qList)]
+		data = [bot.gateway.session.guild(guildID).members[i] for i in bot.gateway.session.guild(guildID).members if i in lastUserIDs]
+		lastName = sorted(set([re.sub(' +', ' ', j['nick'].lower()) if (j.get('nick') and re.sub(' +', ' ', j.get('nick').lower()).startswith(''.join(bot.qList))) else re.sub(' +', ' ', j['username'].lower()) for j in data]))[-1]
+		try:
+			option = lastName[len(bot.qList)]
+			return option
+		except IndexError:
+			return None
+	elif action == 'replace':
+		if bot.qList[-1] in allchars:
+			options = allchars[allchars.index(bot.qList[-1])+1:]
+			if ' ' in options and (len(bot.qList)==1 or (len(bot.qList)>1 and bot.qList[-2]==' ')): #cannot start with a space and cannot have duplicate spaces
+				options.remove(' ')
+			return options
+		else:
+			return None
+
+def findReplaceableIndex(guildID):
+	for i in range(len(bot.qList)-2, -1, -1): #assume that the last index is not changable
+		if bot.qList[i] != '~':
+			return i
+	return None
+
+def bruteForceTest(resp, guildID, wait):
+	if resp.event.ready_supplemental:
+		searchGuildMembers([guildID], query=''.join(bot.qList), limit=100)
+	elif resp.event.guild_members_chunk:
+		remove = False
+		if len(bot.gateway.guildMemberSearches[guildID]["queries"][''.join(bot.qList)]) == 100: #append
+			appendOption = calculateOption(guildID, 'append')
+			if appendOption:
+				bot.qList.append(appendOption)
+			else:
+				remove = True
+		else: #if <100 results returned, replace
+			replaceOptions = calculateOption(guildID, 'replace')
+			if replaceOptions:
+				bot.qList[-1] = replaceOptions[0]
+			else:
+				remove = True
+		if remove: #if no replace options, find first replaceable index & replace it
+			if len(bot.qList) == 1: #reached end of possibilities
+				bot.gateway.removeCommand({"function": bruteForceTest, "params":{"guildID":guildID, "wait":wait}})
+				bot.gateway.close()
+			else:
+				replaceableInd = findReplaceableIndex(guildID)
+				if replaceableInd != None:
+					bot.qList = bot.qList[:replaceableInd+1]
+					replaceOptions = calculateOption(guildID, 'replace')
+					bot.qList[-1] = replaceOptions[0]
+				else:
+					bot.gateway.removeCommand({"function": bruteForceTest, "params":{"guildID":guildID, "wait":wait}})
+					bot.gateway.close()
+		if wait: time.sleep(wait)
+		print('next query: '+''.join(bot.qList))
+		print('members fetched so far: '+str(len(bot.gateway.session.guild(guildID).members)))
+		print("efficiency: "+str(len(bot.gateway.session.guild(guildID).members)/(len(bot.gateway.guildMemberSearches[guildID]["queries"])))+"%")
+		searchGuildMembers([guildID], query=''.join(bot.qList), limit=100)
+
+guildID = ''
+wait = 1
+bot.gateway.command({"function": bruteForceTest, "params":{"guildID":guildID, "wait":wait}})

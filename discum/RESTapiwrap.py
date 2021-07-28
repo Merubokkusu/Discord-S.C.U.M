82c814bef9d026e3b922c56340af7e1b7e677447
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-07-28 14:15:05 -0400

    fixes #125

--- discum/RESTapiwrap.py
+++ discum/RESTapiwrap.py
@@ -99,7 +99,7 @@ class Wrapper:
 			if response and response.headers.get('Content-Encoding') == "br": #decompression; gzip/deflate is automatically handled by requests module
 				response._content = Wrapper.brdecompress(response.content, log)
 			# 8. log response
-			if response:
+			if response != None:
 				text, color = Wrapper.logFormatter(function_name, response.text, part="response")
 				Logger.log(text, color, log)
 			# 9. return response object with decompressed content

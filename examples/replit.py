35a76f4184891b47eef222c842da788e4a5dd09b
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-10-22 00:34:21 -0400

    Update examples/replit.py

--- examples/replit.py
+++ examples/replit.py
@@ -6,3 +6,8 @@ except:
     import os
     os.system("pip install git+https://github.com/Merubokkusu/Discord-S.C.U.M.git#egg=discum")
     import discum
+
+'''
+or, you can put this in your pyproject.toml file:
+discum = { git = "git@github.com:Merubokkusu/Discord-S.C.U.M.git", branch = "master" } 
+'''

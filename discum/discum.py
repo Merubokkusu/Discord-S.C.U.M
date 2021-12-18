dcd25e78782456c9c285d32d477b886a4c303f4b
arandomnewaccount <68355905+arandomnewaccount@users.noreply.github.com>
2021-12-17 23:08:13 -0500

    update searchMessages
    #266 (add sortBy and sortOrder params)
    ty trippy22

--- discum/discum.py
+++ discum/discum.py
@@ -305,8 +305,8 @@ class Client:
 		return imports.Messages(self.discord, self.s, self.log).reply(channelID, messageID, message, nonce, tts, embed, allowed_mentions, sticker_ids, file, isurl)
 
 	#search messages
-	def searchMessages(self, guildID=None, channelID=None, authorID=None, authorType=None, mentionsUserID=None, has=None, linkHostname=None, embedProvider=None, embedType=None, attachmentExtension=None, attachmentFilename=None, mentionsEveryone=None, includeNsfw=None, afterDate=None, beforeDate=None, textSearch=None, afterNumResults=None, limit=None):
-		return imports.Messages(self.discord, self.s, self.log).searchMessages(guildID, channelID, authorID, authorType, mentionsUserID, has, linkHostname, embedProvider, embedType, attachmentExtension, attachmentFilename, mentionsEveryone, includeNsfw, afterDate, beforeDate, textSearch, afterNumResults, limit)
+	def searchMessages(self, guildID=None, channelID=None, authorID=None, authorType=None, mentionsUserID=None, has=None, linkHostname=None, embedProvider=None, embedType=None, attachmentExtension=None, attachmentFilename=None, mentionsEveryone=None, includeNsfw=None, sortBy=None, sortOrder=None, afterDate=None, beforeDate=None, textSearch=None, afterNumResults=None, limit=None):
+		return imports.Messages(self.discord, self.s, self.log).searchMessages(guildID, channelID, authorID, authorType, mentionsUserID, has, linkHostname, embedProvider, embedType, attachmentExtension, attachmentFilename, mentionsEveryone, includeNsfw, sortBy, sortOrder, afterDate, beforeDate, textSearch, afterNumResults, limit)
 
 	#filter searchMessages, takes in the output of searchMessages (a requests response object) and outputs a list of target messages
 	def filterSearchResults(self, searchResponse):
